b8f5608867e9581453792ae5287b12295e278381
SobolevD <soboiev86rus@mail.ru>
2022-02-09 12:55:53 +0400

    Fixed properties example Added functions for read, transformation and save images

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import json
-import numpy as np
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PIL import Image
+from skimage import transform
+from skimage.io import imread, imsave
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def read_parameters(file_path):
+    # Open file with parameters
+    with open(file_path) as json_file:
+        json_data = json.load(json_file)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Log
+    for entry in json_data.keys():
+        print(f'Got parameter "{entry}" with value = "{json_data[entry]}"')
+    return json_data
+
+
+def read_image(file_path):
+    # Open file with image
+    img = imread(file_path)
+    # Log
+    print('Image shape: ', img.shape)
+    return img
+
+
+def affine_transform_image(img, scale_xy, shift_xy, angle):
+    tform = transform.AffineTransform(
+        scale=scale_xy,
+        translation=shift_xy,
+        rotation=angle
+    )
+    # Log
+    print(tform.params)
+    tf_img = transform.warp(img, tform)
+    return tf_img
+
+
+def save_image(img, file_path):
+    # imageio.imwrite(file_path, img)#img.save(file_path, 'png')
+    imsave(file_path, img)
+
+
+parameters = read_parameters('resources/application_properties.json')
+
+v_img = read_image(parameters['source_image_path'])
+v_scale_xy = parameters['scale_XY']
+v_shift_xy = parameters['shift_XY']
+v_angle = parameters['angle']
+
+v_result = affine_transform_image(v_img, v_scale_xy, v_shift_xy, v_angle)
+im = Image.fromarray(v_result, 'RGB')
+
+#fig, ax = plt.subplots()
+#ax.imshow(v_result)
+#show()
+
+save_image(v_result, parameters['destination_image_path'])
